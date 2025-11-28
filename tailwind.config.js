9db6b831c8a5e02680074dbab610c40593d8e89d
UP1992 <panchaludit150@gmail.com>
2025-11-28 09:15:37 +0530

    Setup the react native app

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light:{
+          100: "#D6C6FF",
+          200: "#A8b5db",
+          300: "#9ca4ab",
+        },
+        dark:{
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
+  },
+  plugins: [],
+};
